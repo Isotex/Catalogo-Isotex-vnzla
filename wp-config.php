dc1ae4d275cdf03de12c633cf17269fa27e80623
rdiez <rdiez@screen.com>
2015-01-28 01:06:15 -0430

    Instalacion del tema

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_catalago_isotex_vnzla');
+define('DB_NAME', 'wp_distribuidora_isotex');
 
 /** MySQL database username */
 define('DB_USER', 'UserIsotex');
